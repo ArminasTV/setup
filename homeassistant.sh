55c4e237ed02532e05fe780edb86ef23bc7b9def
ArminasTV <52028186+ArminasTV@users.noreply.github.com>
2019-06-26 18:26:36 +0200

    Create homeassistant.sh

---
+++ homeassistant.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "******************************************************\n"
+echo -e "**************** Installation Home Assistant *********\n"
+echo -e "******************************************************\n"
+
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository universe
+sudo apt-get update
+sudo curl -fsSL get.docker.com | sh
+
+sudo apt-get install -y apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat
+
+sudo curl -sL https://raw.githubusercontent.com/home-assistant/hassio-installer/master/hassio_install.sh | sudo bash -s -- -m raspberrypi3
