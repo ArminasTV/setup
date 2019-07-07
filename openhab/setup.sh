33869c1f7fa2b1e90eb9498f1be9e4484e862f70
ArminasTV <52028186+ArminasTV@users.noreply.github.com>
2019-07-07 19:17:52 +0200

    Update setup.sh

--- openhab/setup.sh
+++ openhab/setup.sh
@@ -6,3 +6,4 @@ influx -execute "CREATE USER openhab WITH PASSWORD 'openhab' WITH ALL PRIVILEGES
 sudo wget https://raw.githubusercontent.com/ArminasTV/setup/master/openhab/services/influxdb.cfg -O /etc/openhab2/services/influxdb.cfg
 sudo wget https://raw.githubusercontent.com/ArminasTV/setup/master/openhab/persistence/influxdb.persist -O /etc/openhab2/persistence/influxdb.persist
 
+sudo /etc/init.d/openhab2 restart
