34bf44c462603417e3a835e06a4460099d861024
ArminasTV <52028186+ArminasTV@users.noreply.github.com>
2019-07-07 18:58:07 +0200

    Create setup.sh

---
+++ openhab/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+influx -execute "CREATE DATABASE openhab"
+influx -execute "CREATE USER openhab WITH PASSWORD 'openhab' WITH ALL PRIVILEGES"
+
+sudo wget https://raw.githubusercontent.com/ArminasTV/setup/master/openhab/services/influxdb.cfg -O /etc/openhab2/services/influxdb.cfg
+sudo wget https://raw.githubusercontent.com/ArminasTV/setup/master/openhab/persistence/influxdb.persist -O /etc/openhab2/persistence/influxdb.persist
+
