1faf223b4080fe9bd0ae49eb1eb026ab3ab8cfcc
ArminasTV <52028186+ArminasTV@users.noreply.github.com>
2019-07-10 18:40:56 +0200

    Update homeassistant.sh

--- homeassistant.sh
+++ homeassistant.sh
@@ -4,8 +4,6 @@ echo -e "******************************************************\n"
 echo -e "**************** Installation Home Assistant *********\n"
 echo -e "******************************************************\n"
 
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository universe
 sudo apt-get update
 sudo curl -fsSL https://raw.githubusercontent.com/ArminasTV/setup/master/get-docker.sh | sh
 sudo usermod -aG docker pi
