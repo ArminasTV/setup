a8749e17bd5bfab7b276b51f8ddc9fd3b6c87ae4
ArminasTV <52028186+ArminasTV@users.noreply.github.com>
2019-07-10 18:40:23 +0200

    Support raspbian 10 buster
    On utilise notre get-docker.sh

--- homeassistant.sh
+++ homeassistant.sh
@@ -7,7 +7,8 @@ echo -e "******************************************************\n"
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository universe
 sudo apt-get update
-sudo curl -fsSL get.docker.com | sh
+sudo curl -fsSL https://raw.githubusercontent.com/ArminasTV/setup/master/get-docker.sh | sh
+sudo usermod -aG docker pi
 
 sudo apt-get install -y apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat
 
