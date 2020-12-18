382b0730f8c83f821711ee481e4d9a2cb33d6f0b
ArminasTV <52028186+ArminasTV@users.noreply.github.com>
2020-12-18 23:34:12 +0100

    Create openhab3.sh
    Setup easy for openhab 3

---
+++ openhab3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash	
+  
+echo -e "******************************************************\n"
+echo -e "**************** Installation Openhab ****************\n"
+echo -e "******************************************************\n"
+wget -qO - 'https://bintray.com/user/downloadSubjectPublicKey?username=openhab' | sudo apt-key add -
+echo 'deb https://dl.bintray.com/openhab/apt-repo2 stable main' | sudo tee /etc/apt/sources.list.d/openhab.list
+
+sudo apt-get update
+ 
+sudo apt-get install -y openjdk-8-jdk-headless
+sudo apt-get install -y apt-transport-https
+
+sudo apt-get install -y openhab
+sudo apt-get install -y openhab-addons
+
+# initialize the new service (execute only once)
+sudo systemctl daemon-reload
+sudo systemctl enable openhab.service
+
+#start and retrieve status
+sudo systemctl start openhab.service
+sudo systemctl status openhab.service
